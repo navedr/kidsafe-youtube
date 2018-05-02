abf53519dea7372f8bb83e0ef3195da695793dcc
Naved Rangwala <navedindia@gmail.com>
2018-05-01 21:33:04 -0700

    styling fixes

--- js/google/init.js
+++ js/google/init.js
@@ -23,7 +23,7 @@ Site = {
     $scope.items = [];
     $scope.onStageItem = null;
     $scope.dataVersion = '0.0';
-    $scope.appVersion = '1.0b13';
+    $scope.appVersion = '1.0';
 
     if (query.DeviceID && query.Device) {
       mixpanel.identify(`${query.Device}-${query.DeviceID}`);
